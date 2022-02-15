7970621c82fe5656e0e48b5fa0610e8f751161db
Vasant <vasantbasutkar143@gmail.com>
2022-02-15 18:57:57 +0530

    second commit

--- src/com/example/java/Methods07_04.java
+++ src/com/example/java/Methods07_04.java
@@ -1,4 +1,31 @@
 package com.example.java;
 
+                      //*** Creating reusable code with methods ***//
+
 public class Methods07_04 {
+
+        static String[] months ={"January", "February", "March",
+                "April", "May", "June", "July",
+                "August", "September", "October",
+                "November", "December"};
+
+    public static void main(String[] args) {
+        loopIt("Months of The Year");
+        loopIt("Second Run");
+        loopIt("Third Run");
+    }
+    static void loopIt(String label){
+
+        System.out.println(label);
+        for (int i = 0; i < label.length(); i++) {
+            System.out.print("*");
+        }
+        System.out.println("");
+
+        for (int i=0; i< months.length; i++) {
+            System.out.println(months[i]);
+        }
+
+    }
+
 }
