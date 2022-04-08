e10c4022d3ee917a1a4cf48617a97cb2fa0c9db0
Vasant <vasant.basutkar@craveinfotech.com>
2022-04-08 15:31:16 +0530

    Added crave pkg

---
+++ src/craveAssignments/StringDemo.java
@@ -0,0 +1,35 @@
+package craveAssignments;
+
+
+    // Creating Strings
+/* public class StringDemo {
+
+    public static void main(String args[]) {
+        char[] helloArray = { 'h', 'e', 'l', 'l', 'o', '.' };
+        String helloString = new String(helloArray);
+        System.out.println( helloString );
+    }
+}
+*/
+
+
+       // String Length
+/* public class StringDemo {
+
+    public static void main(String args[]) {
+        String palindrome = "Dot saw I was Tod";
+        int len = palindrome.length();
+        System.out.println( "String Length is : " + len );
+    }
+} */
+
+      // Concatenating Strings
+
+
+public class StringDemo {
+
+    public static void main(String args[]) {
+        String string1 = "saw I was ";
+        System.out.println("Dot " + string1 + "Tod");
+    }
+}
