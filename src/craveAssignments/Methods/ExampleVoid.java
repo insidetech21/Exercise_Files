ad7af48b9a1ed2e4331c66fce9771b4cb4ad5ede
Vasant <vasant.basutkar@craveinfotech.com>
2022-04-08 17:52:00 +0530

    new programs added in crave pkg

---
+++ src/craveAssignments/Methods/ExampleVoid.java
@@ -0,0 +1,18 @@
+package craveAssignments.Methods;
+
+public class ExampleVoid {
+
+    public static void main(String[] args) {
+        methodRankPoints(25.7);
+    }
+
+    public static void methodRankPoints(double points) {
+        if (points >= 202.5) {
+            System.out.println("Rank:A1");
+        }else if (points >= 122.4) {
+            System.out.println("Rank:A2");
+        }else {
+            System.out.println("Rank:A3");
+        }
+    }
+}
